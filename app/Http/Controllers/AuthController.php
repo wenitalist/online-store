b21606e70c4d5bf551dbb15539e012d91fdb69d3
wenitalist <wenitalist@gmail.com>
2024-03-27 14:48:30 +0300

    user registration method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class AuthController
@@ -19,7 +21,22 @@ class AuthController
         
     }
 
-    public function newUser() {
+    public function createUser(Request $request) {
 
+        $request->validate([
+            'login' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|string|min:6|max:255',
+        ]);
+
+        $newModelUser = new User();
+
+        $newModelUser->login = $request->input('login');
+        $newModelUser->email = $request->input('email');
+        $newModelUser->password = $request->input('password');
+
+        $newModelUser->save();
+
+        return redirect()->route('mainPage');
     }
 }
