3e7b649c19899098737c98201840bd8f8590e940
wenitalist <wenitalist@gmail.com>
2024-03-27 14:47:20 +0300

    update html and css

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -10,11 +10,11 @@
 <body>
     <div class='div-registration'>
         <p>Регистрация</p>
-        <form class='form-registration' method='POST' action='registration/new-user'>
+        <form class='form-registration' method='POST' action='registration/create-user'>
             @csrf
-            <input class='input-text' type="text" id='login' required placeholder="Логин">
-            <input class='input-text' type="email" id='mail' required placeholder="Почта">
-            <input class='input-text' type="password" id='password' required placeholder="Пароль">
+            <input class='input-text' type="text" id='login' name='login' required placeholder="Логин">
+            <input class='input-text' type="email" id='email' name='email' required placeholder="Почта">
+            <input class='input-text' type="password" id='password' name='password' required placeholder="Пароль">
             <input class='input-submit' type="submit" value="Зарегистрироваться">
         </form>
         <a href='/authorization'>Войти в аккаунт</a>
