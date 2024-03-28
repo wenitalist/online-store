d71ede10059002af4f38a77c1e16f92ed21ad4e2
wenitalist <wenitalist@gmail.com>
2024-03-28 14:07:57 +0300

    messages for incorrect data

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     switch (window.location.pathname) {
         case '/registration':
             listenerForFormRegistration('form-registration');
@@ -10,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function listenerForFormRegistration(formId) {
-
         const form = document.getElementById(formId);
 
         form.addEventListener('submit', async function(event) {
@@ -29,24 +27,56 @@ document.addEventListener('DOMContentLoaded', function() {
 
             response = await fetch(request);
 
-            if (response.status === 422) {
-
-                data = await response.json();
-
+            if (temp = checkStatus(response.status)) {
                 elementForMessage.style.cssText = 'display: block;'
+                elementForMessage.textContent = temp;
 
-                if (data.errors.email) { elementForMessage.textContent = 'Почта занята' }
-                if (data.errors.login) { elementForMessage.textContent = 'Логин занят' }
-                if (data.errors.password) { elementForMessage.textContent = 'Пароль должен содержать от 6 символов' }
+                // if (data.errors.email) { elementForMessage.textContent = 'Почта занята' }
+                // if (data.errors.login) { elementForMessage.textContent = 'Логин занят' }
+                // if (data.errors.password) { elementForMessage.textContent = 'Пароль должен содержать от 6 символов' }
 
             } else {
-
                 window.location.href = '/authorization';
             }
         });
     }
 
-    function listenerForFormRegistration(formId) {
+    function listenerForFormAuthorization(formId) {
+        const form = document.getElementById(formId);
 
+        form.addEventListener('submit', async function(event) {
+    
+            event.preventDefault();
+
+            elementForMessage = document.getElementById('auth-error-message');
+    
+            const request = new Request(this.action, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json'
+                },
+                body: new FormData(this)
+            });
+
+            response = await fetch(request);
+
+            if (temp = checkStatus(response.status)) {
+                elementForMessage.style.cssText = 'display: block;'
+                elementForMessage.textContent = temp;
+            } else {
+                window.location.href = '/';
+            }
+        });
+    }
+
+    function checkStatus(status) {
+        switch (status) {
+            case 422:
+                return 'Неправильный формат входных данных';
+            case 401:
+                return 'Неверный логин или пароль';
+            default:
+                return false;
+        }
     }
 });
