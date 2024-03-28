20e6ad58d709fc82d29e14583230c9e4468710ff
wenitalist <wenitalist@gmail.com>
2024-03-28 13:33:32 +0300

    middlewares for routes

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'show'])->name('main-page');
 
-Route::get('/authorization', [AuthController::class, 'showAuthorizationPage'])->name('authorization-page');
-Route::get('/registration', [AuthController::class, 'showRegistrationPage'])->name('registration-page');
+Route::middleware('auth')->group(function () { 
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
 
-Route::get('/logout', [AuthController::class, 'logout']);
+Route::middleware('guest')->group(function () { 
+    Route::get('/authorization', [AuthController::class, 'showAuthorizationPage'])->name('authorization-page');
+    Route::get('/registration', [AuthController::class, 'showRegistrationPage'])->name('registration-page');
 
-Route::post('/authorization/login', [AuthController::class, 'login']);
-Route::post('/registration/create-user', [AuthController::class, 'createUser']);
+    Route::post('/authorization/login', [AuthController::class, 'login']);
+    Route::post('/registration/create-user', [AuthController::class, 'createUser']);
+});
 
 // Route::get('/support', [SupportController::class, 'show']);
 // Route::get('/contacts', [ContactsController::class, 'show']);
