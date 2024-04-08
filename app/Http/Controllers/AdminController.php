6544e64d713bedd398508b5613460e24885b16da
wenitalist <wenitalist@gmail.com>
2024-04-08 17:18:09 +0300

    create new supplier

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,15 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Supplier;
 use Illuminate\Http\Request;
 
 class AdminController
 {
     public function showCreateNewProductPage() {
-        return view('admin-panel.new-product');
+        $suppliers = Supplier::all();
+
+        return view('admin-panel.new-product', ['suppliers' => $suppliers]);
+    }
+
+    public function showCreateNewSupplierPage() {
+        return view('admin-panel.new-supplier');
     }
 
     public function createNewProduct() {
         // Сохранение нового товара
+    }
+
+    public function createNewSupplier(Request $request) {
+        if (!Supplier::where('name', $request->input('supplier-name'))->first()) {
+            $supplier = new Supplier();
+
+            $supplier->name = $request->input('supplier-name');
+    
+            $supplier->save();
+    
+            return redirect()->route('new-supplier');
+        }
+
+        return response()->json(['error' => 'Supplier name has already occupied'], 422);
     }
 }
