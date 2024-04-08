e174f324ead6b1732fe93f0eb99881e0edb8e3bc
wenitalist <wenitalist@gmail.com>
2024-04-08 16:03:48 +0300

    add new product page

--- resources/views/admin-panel/index.blade.php
+++ resources/views/admin-panel/index.blade.php
@@ -3,12 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <link rel="icon" type="image/x-icon" href="favicon20x20.ico">
-    <script src='js/admin-panel-script.js'></script>
+    {{-- <script src='js/admin-panel-script.js'></script> --}}
     <title>@yield('title')</title>
 </head>
 <body>
-    @yield('action')
+    <div class='admin-main-div'>
+        <div class='admin-info'>
+            <p>Админ панель</p>
+            <a href='/'>Вернуться на главную</a>
+        </div>
+        <div class='admin-menu-action-section'>
+            <div class='admin-menu'>
+                <a href='/admin/new-product' class='admin-menu-link'>Добавить товар</a>
+                <a href='#' class='admin-menu-link'>Редактировать товар</a>
+                <a href='/admin/new-supplier' class='admin-menu-link'>Добавить поставщика</a>
+                <a href='/admin/categories' class='admin-menu-link'>Категории</a>
+            </div>
+            <div class='action-section'>
+                @yield('action')
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/admin-panel/new-product.blade.php
+++ resources/views/admin-panel/new-product.blade.php
@@ -1,7 +1,25 @@
 @extends('admin-panel.index')
 
-@section('title', 'Создание товара')
+@section('title', 'Добавление товара')
 
 @section('action')
-
-@section
+<form action='/admin/new-product/save' method='POST'>
+    <div class='div-for-form-elements'>
+        <div class='form-inputs-text-div'>
+            <input type='text' placeholder='Название'>
+            <input type='text' placeholder='Описание'>
+            <input type='text' placeholder='Цена'>
+            <input type='text' placeholder='Кол-во'>
+        </div>
+        <div class='form-select-submit-elements'>
+            <select name='supplier-id'> {{-- Надо будет получать всех поставщиков из бд и добавлять их --}}
+                <option disabled selected hidden>Поставщик</option> 
+                <option value='2'>Поставщик 1</option>
+                <option value='3'>Поставщик 2</option>
+                <option value='4'>Поставщик 3</option>
+            </select>
+            <input type='submit' value='Добавить'>
+        </div>
+    </div>
+</form>
+@endsection
