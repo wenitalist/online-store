7b9d511bf338c451a1ec61af136ad620375c59b6
wenitalist <wenitalist@gmail.com>
2024-04-10 12:40:45 +0300

    create new category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use App\Models\Supplier;
 use Illuminate\Http\RedirectResponse;
@@ -19,13 +20,19 @@ class AdminController
         return view('admin-panel.new-supplier');
     }
 
-    public function createNewProduct(Request $request) {
+    public function showCategoriesPage() {
+        $categories = Category::all();
+
+        return view('admin-panel.categories', ['categories' => $categories]);
+    }
+
+    public function createNewProduct(Request $request): RedirectResponse {
         $request->validate([
-            'name' => 'string',
+            'name' => 'required|string',
             'description' => 'string|nullable',
-            'price' => 'integer',
-            'quantity' => 'integer',
-            'supplier' => 'string',
+            'price' => 'required|integer',
+            'quantity' => 'required|integer',
+            'supplier' => 'required|string',
         ]);
 
         $product = new Product();
@@ -53,5 +60,19 @@ class AdminController
         $supplier->save();
     
         return redirect()->route('new-supplier');
+    }
+
+    public function createNewCategory(Request $request): RedirectResponse {
+        $request->validate([
+            'category-name' => 'required|string|unique:categories,name',
+        ]);
+
+        $category = new Category();
+
+        $category->name = $request->input('category-name');
+
+        $category->save();
+
+        return redirect()->route('categories');
     }
 }
